3de5d84bec032986bf37684a80e30a60633c1992
Joey Zhou <90877229+melllllkk@users.noreply.github.com>
2023-05-24 13:12:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,28 +18,33 @@ d5.addEventListener("mouseover", d5func);
 
 function d1func() {
     document.getElementById("ex-title").innerHTML = "AP CSA";
+    document.getElementById("ex-date").innerHTML = "August, 2020 - June, 2021";
     document.getElementById("ex-text").innerHTML = "My initial exposure to programming occurred during my sophomore year of high school when I enrolled in AP CSA. Although grasping the fundamental principles of programming was not particularly challenging, I had difficulty comprehending the underlying logic essential for writing functional programs. I struggled with effectively employing the concepts to develop programs capable of solving problem sets. Consequently, during the AP exam, I was unable to provide answers to any of the free reponse questions due to my lack of understanding regarding the tasks they demanded. This marked the first instance in which I felt genuinely defeated during an examination. Nevertheless, this setback served as motivation for me to strive for a deeper understanding in programming.";
 }
 
 function d2func() {
     document.getElementById("ex-title").innerHTML = "QxQ: Intro to Quantum Computing"
+    document.getElementById("ex-date").innerHTML = "October, 2021 - April, 2022";
     document.getElementById("ex-text").innerHTML = "I initially participated in the Qubit x Qubit (QxQ) program for my college transcript. However, through QxQ, I was able to learn about the different facets of programming and the future of Computer Science. I enjoyed learning about the fundamentals of Quantum Mechanicss and how it is integrated into transforming the future of computing. I hope to pursue quantum computing in the future.";
 
 }
 
 function d3func() {
     document.getElementById("ex-title").innerHTML = "Carnegie Mellon PreCollege"
+    document.getElementById("ex-date").innerHTML = "July 2022 - August, 2022";
     document.getElementById("ex-text").innerHTML = "During the summer of 2022, I was given the opportunity to study at Carnegie Mellon University on a full-ride scholarship. I studied the Fundamentals of Computing (15-112) and Physics for Engineering Students I (33-141). Through 15-112, I was able to extensively study Python through complex problem sets and game development using the tkinter toolkit."
 }
 
 function d4func() {
     document.getElementById("ex-title").innerHTML = "Girls Who Code SIP"
+    document.getElementById("ex-date").innerHTML = "July 2022 - August, 2022";
     document.getElementById("ex-text").innerHTML = "I completed a self-paced summer course to develop proficiency in HTML, CSS, and Javascript. Through this program, I was introduced to the world of web development."
 
 }
 
 function d5func() {
     document.getElementById("ex-title").innerHTML = "Programming Tutor"
+    document.getElementById("ex-date").innerHTML = "October 2022 - present";
     document.getElementById("ex-text").innerHTML = "I am currently working as a paid programming tutor. I conduct biweekly lessons with 5th graders to facilitate the discovery of programming fundamentals through block coding and Python."
 
 }
@@ -68,3 +73,4 @@ function img2func() {
 function img3func() {
     document.getElementById("port-desc").innerHTML = "I coded Tetris for a homework assignment. Click the image for the source code.";
 }
+
